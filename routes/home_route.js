30c0209ac61c293326f4b50a621448c09b3a86d3
Fady <fadymbt@gmail.com>
2021-07-22 16:22:02 +0200

    trying catch error

--- routes/home_route.js
+++ routes/home_route.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // res.redirect('/homepage');
-  res.send('Why are you here?!')
+  try {
+    res.send('Why are you here?!');
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
