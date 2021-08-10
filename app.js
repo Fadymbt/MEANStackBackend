1c7d3f6362329b8c849eb4d639e9d33c4d1428c9
Fady <fadymbt@gmail.com>
2021-08-10 15:51:34 +0200

    added more cors middleware settings

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ mongoose.connection.on('error', () => {
 
 // Cross-Origin middleware to help with cross platform requests
 app.use(cors({origin :true, credentials: true}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // logging HTTP requests
 app.use(morgan('dev'));
